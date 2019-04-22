5e0700e44f5ba05b2e1012d8b1d16e496c6799d8
Tianyijian <2624132357@qq.com>
2019-04-22 10:43:08 +0800

    随机生成数据

---
+++ src/main/Main.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.SecureRandom;
+
+public class Main {
+	public static void main(String[] args) {
+		genData("src/main/data.txt");
+	}
+	
+	public static void genData(String path) {
+		long begin = System.currentTimeMillis();
+
+		StringBuilder res = new StringBuilder();
+		SecureRandom random = new SecureRandom();
+		for (int i = 0; i<1000000;i++) {
+			int A = random.nextInt(1000000);
+			String B = "abcdef";
+			res.append(A +" " + B + "\n");
+		}
+		try {
+			FileOutputStream out = new FileOutputStream(new File(path));
+			BufferedOutputStream bout = new BufferedOutputStream(out);
+			bout.write(res.toString().getBytes());
+			bout.flush();
+			bout.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("Use Time: " + (end - begin) / 1000.0);
+	}
+}
